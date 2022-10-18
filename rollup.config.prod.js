6e6931fec32275b67b15c7b0cb1fa70bab7017e1
yang <2636098325@qq.com>
2022-10-18 17:12:26 +0800

    :ambulance::ambulance::ambulance:

---
+++ rollup.config.prod.js
@@ -0,0 +1,40 @@
+import config from  './rollup.config.js'
+// // const config =require('./rollup.config')/
+// const path = require('path')
+// import { terser } from "rollup-plugin-terser";
+// // import analyze from 'rollup-plugin-analyzer'
+// const rimraf = require('rimraf');
+// rimraf('./dist/*',(err)=>{
+//     if(err){
+//         console.error(err)
+//     }else{
+//         console.log('Garbage removal program'.blue)
+//     }
+// })
+
+// let output={
+//     sourcemap:false,
+//     compact:true,
+//     file: path.join(__dirname, './dist/index.js'),
+
+// }
+// config.output=Object.assign({},config.output,output)
+// config.plugins=config.plugins.concat([
+//     terser({
+//         compress: {
+//             drop_console: true,
+//             drop_debugger: false,
+//             pure_funcs: ['console.log'] // 移除console
+//         }
+//     }),
+//     // myExample()
+//     // analyze()
+// ])
+
+
+
+// console.log(config)
+// console.log(config.output)
+
+export default config
+
